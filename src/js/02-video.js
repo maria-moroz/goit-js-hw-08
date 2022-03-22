6a3d464022a26b48aab508250588d9ee27bf5c50
maria-moroz <m0954008202@gmail.com>
2022-03-22 21:47:54 +0200

    Update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(currentTime) {
+    localStorage.setItem(STORAGE_KEY, currentTime.seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    formEmailField: document.querySelector('.feedback-form input'),
+    formMessageField: document.querySelector('.feedback-form textarea'),
+}
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+fillFormFieldsOnReset(savedData);
+
+function fillFormFieldsOnReset(data) {
+    if (!data) {
+        return;
+    }
+
+    if (data.hasOwnProperty('email')) {
+        refs.formEmailField.value = data.email;
+    }
+
+    if (data.hasOwnProperty('message')) {
+        refs.formMessageField.value = data.message;
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+        email: refs.formEmailField.value,
+        message: refs.formMessageField.value,
+    }
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+    console.log(data);
+}
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY , JSON.stringify(formData));
+}
